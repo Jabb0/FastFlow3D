644a61f9860ff9bf348e7dd2064759d890364dcb
aron <aron.distelzweig@hotmail.com>
2021-07-12 20:55:50 +0200

    add comments for baseline layers

--- networks/flownet3d/layers.py
+++ networks/flownet3d/layers.py
@@ -9,7 +9,15 @@ from networks.flownet3d.util import build_shared_mlp
 
 class SetConvLayer(nn.Module):
     """
-    SetConvLayer
+    SetConvLayer of FlowNet3D:
+        Takes a point cloud as input and samples points (called regions) from it by using furthest point sampling (FPS).
+        For each sampled point/region, it aggregates over the features of the points of the input point cloud, which are
+        within the given radius. The aggregated features are now the new features for the sampled point/region.
+
+    References
+    ----------
+    .. FlowNet3D: Learning Scene Flow in 3D Point Clouds: Xingyu Liu, Charles R. Qi, Leonidas J. Guibas
+       https://arxiv.org/pdf/1806.01411.pdf
     """
     def __init__(self, sample_rate: float, radius: float, n_samples: int, mlp: List[int],
                  bn: bool = True, use_xyz: bool = True):
@@ -36,9 +44,9 @@ class SetConvLayer(nn.Module):
         Returns
         -------
         new_xyz : torch.Tensor
-            (B, n_points, 3) tensor of the new features' xyz
+            (B, N * sample_rate, 3) tensor of the new features' xyz
         new_features : torch.Tensor
-            (B,  \sum_k(mlp[k][-1]), n_points) tensor of the new_features descriptors
+            (B,  \sum_k(mlp[k][-1]), N * sample_rate) tensor of the new_features descriptors
         """
         n_points = int(pos.shape[1] * self.sample_rate)
 
@@ -59,7 +67,15 @@ class SetConvLayer(nn.Module):
 
 class FlowEmbeddingLayer(nn.Module):
     """
-    SetConvLayer
+    FlowEmbeddingLayer of FlowNet3D:
+        Takes two point clouds as input and aggregates for each point, in the first point cloud, the features of the
+        points in the second point cloud, which are within a given radius w.r.t the point of the first point cloud.
+        The aggregated features are now the new features for the point in the first point cloud.
+
+    References
+    ----------
+    .. FlowNet3D: Learning Scene Flow in 3D Point Clouds: Xingyu Liu, Charles R. Qi, Leonidas J. Guibas
+       https://arxiv.org/pdf/1806.01411.pdf
     """
     def __init__(self, sample_rate: float, radius: float, n_samples: int, mlp: List[int],
                  bn: bool = True, use_xyz: bool = True):
@@ -79,17 +95,21 @@ class FlowEmbeddingLayer(nn.Module):
         r"""
         Parameters
         ----------
-        pos : torch.Tensor
-            (B, N, 3) tensor of the xyz coordinates of the features
-        features : torch.Tensor
-            (B, C, N) tensor of the descriptors of the the features
+        pos1 : torch.Tensor
+            (B, N1, 3) tensor of the xyz coordinates of the features
+        features1 : torch.Tensor
+            (B, C, N1) tensor of the descriptors of the the features
+        pos2 : torch.Tensor
+            (B, N2, 3) tensor of the xyz coordinates of the features
+        features2 : torch.Tensor
+            (B, C, N2) tensor of the descriptors of the the features
 
         Returns
         -------
         new_xyz : torch.Tensor
-            (B, n_points, 3) tensor of the new features' xyz
+            (B, N2, 3) tensor of the new features' xyz
         new_features : torch.Tensor
-            (B,  \sum_k(mlp[k][-1]), n_points) tensor of the new_features descriptors
+            (B,  \sum_k(mlp[k][-1]), N2) tensor of the new_features descriptors
         """
 
         new_features = self.grouper(pos1, pos2, features1, features2)  # (B, C, n_points, n_samples)
@@ -105,7 +125,17 @@ class FlowEmbeddingLayer(nn.Module):
 
 class SetConvUpLayer(nn.Module):
     """
-    SetConvLayer
+    SetConvUpLayer of FlowNet3D:
+        Takes two point clouds as input (also called source and target) and propagates the features from the source
+        point cloud to the target point cloud.
+        This feature propagation is done by aggregating all features in the source point cloud for each point
+        in the target point cloud, only features of points are aggregated which are within the given radius w.r.t.
+        the point in the target point cloud.
+
+    References
+    ----------
+    .. FlowNet3D: Learning Scene Flow in 3D Point Clouds: Xingyu Liu, Charles R. Qi, Leonidas J. Guibas
+       https://arxiv.org/pdf/1806.01411.pdf
     """
     def __init__(self, sample_rate: float, radius: float, n_samples: int, mlp: List[int],
                  bn: bool = True, use_xyz: bool = True):
@@ -125,17 +155,21 @@ class SetConvUpLayer(nn.Module):
         r"""
         Parameters
         ----------
-        pos : torch.Tensor
-            (B, N, 3) tensor of the xyz coordinates of the features
-        features : torch.Tensor
-            (B, C, N) tensor of the descriptors of the the features
+        src_pos : torch.Tensor
+            (B, N1, 3) tensor of the xyz coordinates of the features
+        src_features : torch.Tensor
+            (B, C, N1) tensor of the descriptors of the the features
+        target_pos : torch.Tensor
+            (B, N2, 3) tensor of the xyz coordinates of the features
+        target_features : torch.Tensor
+            (B, C, N2) tensor of the descriptors of the the features
 
         Returns
         -------
-        new_xyz : torch.Tensor
-            (B, n_points, 3) tensor of the new features' xyz
+        target_pos : torch.Tensor
+            (B, N2, 3) tensor of the new features' xyz
         new_features : torch.Tensor
-            (B,  \sum_k(mlp[k][-1]), n_points) tensor of the new_features descriptors
+            (B,  \sum_k(mlp[k][-1]), N2) tensor of the new_features descriptors
         """
 
         new_features = self.grouper(src_pos, target_pos, src_features)  # (B, C, n_points, n_samples)
