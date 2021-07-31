68d5a37d140107f135de8d3668fee97dbe160a0b
aron <aron.distelzweig@hotmail.com>
2021-07-31 13:16:17 +0200

    add network profiling

---
+++ profiling.py
@@ -0,0 +1,58 @@
+import numpy as np
+import torch
+
+from models.Flow3DModel import Flow3DModel
+from models.FastFlow3DModelScatter import FastFlow3DModelScatter
+
+torch.manual_seed(0)
+
+
+def create_random_data(n_points, n_features):
+    pc_pos = torch.randint(low=-85, high=85, size=(1, n_points, 3)).cuda()
+    pc_f = torch.randn(size=(1, n_points, n_features)).cuda()
+    pc = torch.cat([pc_f, pc_pos], dim=-1)
+    grid = torch.randint(low=0, high=10000, size=(1, n_points)).cuda()
+    mask = torch.ones(size=(1, n_points)).long().cuda()
+    return pc, grid, mask
+
+
+def run():
+    import time
+    points = [20000]
+    architectures = ['FlowNet']
+    n_forward_passes = 10
+
+    f = open('timing.txt', 'w')
+    for arch in architectures:
+        s = "Time measurement for architecture {}:".format(arch)
+        for n_points in points:
+            if arch == 'FastFlowNet':
+                model = FastFlow3DModelScatter(n_pillars_x=512, n_pillars_y=512).cuda()
+                n_features = 5
+            elif arch == 'FlowNet':
+                model = Flow3DModel().cuda()
+                n_features = 2
+            else:
+                raise ValueError("Unknown architecture {}".format(arch))
+
+            times = list()
+            for i in range(n_forward_passes):
+                prev_pc = create_random_data(n_points, n_features=n_features)
+                cur_pc = create_random_data(n_points, n_features=n_features)
+                x = (prev_pc, cur_pc)
+                t = time.time()
+                model(x)
+                elapsed_time = time.time() - t
+                times.append(elapsed_time * 1000)  # convert sec to ms
+
+            mean_time = np.mean(np.array(times))
+            s += "\n\t{}: ".format(n_points) + "{:.2f}ms".format((float(mean_time)))
+            f.write(s)
+        print(s)
+    f.close()
+
+
+if __name__ == '__main__':
+    run()
+
+
